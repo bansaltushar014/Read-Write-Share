820b89e5d8112253b3886e7eca64ae762135b177
bansaltushar014 <bansaltushar014@gmail.com>
2020-01-22 09:22:02 +0530

    view

--- public/Angular/index.js
+++ public/Angular/index.js
@@ -2,42 +2,42 @@
 angular.module('rewrsh', []);
 
 var Socketdata = function ($scope, servicefun) {
-    
-    
-    servicefun
-    .success(function(data){
-        $scope.data = servicefun;
-    })
-    .error(function(e){
-        console.log(e);
-    });
+    console.log("can not see this");
+    $scope.data = servicefun;
+    // servicefun
+    // .success(function(data){
+    //     $scope.data = servicefun;
+    // })
+    // .error(function(e){
+    //     console.log(e);
+    // });
 };
 
-// var servicefun = function () {
-//     return [{
-//         "_id": {
-//             "$oid": "5d1af37a362f8f148b7dbe15"
-//         },
-//         username: "Rahul Kumar",
-//         "email": "kumar@gmail.com",
-//         "password": "$2b$05$KupEam4dV/Fq7ge3ZnIsVu7iqghvidqEh1jYhc.INtV.gECBJ/sk2",
-//         "__v": 0
-//     },{
-//         "_id": {
-//             "$oid": "5ca949d4100d020017c3051d"
-//         },
-//         username: "User gghh",
-//         "email": "gghh@w",
-//         "password": "$2b$05$7R8/TemxCgtqSUwQwAeySOqMHlJ3AwNvf72dwJmMt6Kv0z4Jd1rd2",
-//         "__v": 0
-//     }];
-//     };
-
-
- var servicefun = function ($http) {
-    console.log("working");
-    return $http.get('https://floating-stream-61460.herokuapp.com/api/signup');
+var servicefun = function () {
+    return [{
+        "_id": {
+            "$oid": "5d1af37a362f8f148b7dbe15"
+        },
+        username: "Rahul Kumar",
+        "email": "kumar@gmail.com",
+        "password": "$2b$05$KupEam4dV/Fq7ge3ZnIsVu7iqghvidqEh1jYhc.INtV.gECBJ/sk2",
+        "__v": 0
+    },{
+        "_id": {
+            "$oid": "5ca949d4100d020017c3051d"
+        },
+        username: "User gghh",
+        "email": "gghh@w",
+        "password": "$2b$05$7R8/TemxCgtqSUwQwAeySOqMHlJ3AwNvf72dwJmMt6Kv0z4Jd1rd2",
+        "__v": 0
+    }];
     };
+
+
+//  var servicefun = function ($http) {
+//     console.log("working");
+//     return $http.get('https://floating-stream-61460.herokuapp.com/api/signup');
+//     };
     
 
 
